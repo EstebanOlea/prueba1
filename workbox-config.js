10cb3369ce61716b9e75233dc4e19c450c31ed34
EstebanOlea <114716953+EstebanOlea@users.noreply.github.com>
2022-12-01 06:00:22 -0600

    Initial commit

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'C:\Users\esteb\OneDrive\Escritorio\prueba',
+	globPatterns: [
+		'**/*.{jpg,html,json,js,css}'
+	],
+	swDest: 'C:\Users\esteb\OneDrive\Escritorio\prueba/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
